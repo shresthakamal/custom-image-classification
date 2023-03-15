3985878d2a23559dcf1a1a79f58b3a7dfdce326c
kamal-shrestha <kamalandshrestha@gmail.com>
2023-03-15 11:55:57 +0545

    Predict Pipe working

--- lglutide/predict.py
+++ lglutide/predict.py
@@ -1,6 +1,7 @@
 import torch
 import torchvision.transforms as transforms
 from torchvision.io import read_image
+from torchvision.transforms import ToTensor
 
 from lglutide import config
 from lglutide.nn import NNModel
@@ -11,14 +12,18 @@ def predict(image):
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
     image = transforms.Resize((config.IMAGE_W, config.IMAGE_H))(image)
+    # convert image to numpy array
+    image = image.numpy()
 
+    image = transforms.ToTensor()(image)
     image = image.to(device)
+    image = image.reshape(-1, config.IMAGE_C, config.IMAGE_H, config.IMAGE_W)
 
     model = NNModel()
 
     model = model.to(device)
 
-    model.load_state_dict(torch.load("lglutide/models/model.pth"))
+    model.load_state_dict(torch.load("lglutide/models/model_4.pth"))
 
     # set the model to evaluation mode
     model.eval()
@@ -32,15 +37,15 @@ def predict(image):
 
     # print the class
     if preds == 0:
-        print("Uninfected")
+        print("U")
     else:
-        print("Infected")
+        print("A")
 
 
 # python main block
 if __name__ == "__main__":
-    # img_path = input("Enter the image path: ")
-    img_path = "data/A/A1.jpeg"
+    img_path = input("Enter the image path: ")
+    # img_path = "data/A/A1.jpeg"
     image = read_image(img_path)
 
     predict(image)
